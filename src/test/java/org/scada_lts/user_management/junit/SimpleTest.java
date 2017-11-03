c4f2133d0356328a8543233315c65d0cb5b0f775
grzesiek <grzegorz.bylica@gmail.com>
2017-11-03 19:42:54 +0100

    #3 Initial version integration with coveralls

---
+++ src/test/java/org/scada_lts/user_management/junit/SimpleTest.java
@@ -0,0 +1,16 @@
+package org.scada_lts.user_management.junit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(JUnit4.class)
+public class SimpleTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
