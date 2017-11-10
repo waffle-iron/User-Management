6624c2d68f1e67d75ad8333c0b7dafcec18e8542
grzesiek <grzegorz.bylica@gmail.com>
2017-11-10 14:35:56 +0100

    #12 next initial version correcting test mock

--- src/test/java/org/scada_lts/user_management/e2e/AuthenticationApiTest.java
+++ src/test/java/org/scada_lts/user_management/e2e/AuthenticationApiTest.java
@@ -1,15 +1,21 @@
 package org.scada_lts.user_management.e2e;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.scada_lts.user_management.model.dao.User;
 import org.scada_lts.user_management.model.dto.UserDto;
 import org.scada_lts.user_management.service.AuthenticationService;
 import org.scada_lts.user_management.service.UsersService;
+import org.scada_lts.user_management.web.api.AuthenticationAPI;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import javax.annotation.Resource;
 
@@ -25,14 +31,25 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class AuthenticationApiTest {
 
-    @Resource
+    @Mock
     private AuthenticationService authService;
 
-    @Resource
+    @Mock
     private UsersService usersService;
 
-    @Resource
+    @InjectMocks
+    private AuthenticationAPI authApi;
+
+    //@Resource
     private MockMvc mockMvc;
+
+    @Before
+    public void init(){
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(authApi)
+                .build();
+    }
     
     @Test
     public void getTokenUser() throws Exception {
