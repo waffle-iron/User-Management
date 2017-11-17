161b57c0adfea546f5cdd5917c11234698cb60f7
Arkadiusz Parafiniuk <arkadiusz.parafiniuk@gmail.com>
2017-11-17 11:46:38 +0100

    Swagger2 API header added

--- src/main/java/org/scada_lts/user_management/SwaggerConfig.java
+++ src/main/java/org/scada_lts/user_management/SwaggerConfig.java
@@ -2,8 +2,10 @@ package org.scada_lts.user_management;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,6 +19,15 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/docs")
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("User Management")
+                .description("User Management API Console")
                 .build();
     }
 }
