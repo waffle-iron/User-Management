61eacfb54cd150089c381743b51d2ed12267b40c
Arkadiusz Parafiniuk <arkadiusz.parafiniuk@gmail.com>
2017-11-17 16:15:55 +0100

    Entity class service interface

---
+++ src/main/java/org/scada_lts/user_management/service/acl/EntityClassServiceImpl.java
@@ -0,0 +1,58 @@
+/*
+ * (c) 2017 Abil'I.T. http://abilit.eu/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.scada_lts.user_management.service.acl;
+
+import org.scada_lts.user_management.model.acl.EntityClass;
+
+import java.util.List;
+
+/**
+ * Class created by Arkadiusz Parafiniuk
+ * E-mail: arkadiusz.parafiniuk@gmail.com
+ */
+public class EntityClassServiceImpl implements EntityClassService {
+    @Override
+    public List<EntityClass> getAll() {
+        return null;
+    }
+
+    @Override
+    public EntityClass add(String className) {
+        return null;
+    }
+
+    @Override
+    public void del(EntityClass entityClass) {
+
+    }
+
+    @Override
+    public EntityClass getEntityClass(Long id) {
+        return null;
+    }
+
+    @Override
+    public EntityClass getEntityClass(String className) {
+        return null;
+    }
+
+    @Override
+    public void update(EntityClass entityClass) {
+
+    }
+}
